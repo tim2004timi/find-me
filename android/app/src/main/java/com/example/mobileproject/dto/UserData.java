9dcfb789cc920ef5180c39b431d1a720a4893cc5
azmodessu <golovrkd@yandex.ru>
2024-03-28 23:24:39 +0300

    Design edited, JSON convert

---
+++ android/app/src/main/java/com/example/mobileproject/dto/UserData.java
@@ -0,0 +1,51 @@
+package com.example.mobileproject.dto;
+
+public class UserData {
+    public String username;
+    public String status;
+    public String age;
+    public String sex;
+    public String city;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
