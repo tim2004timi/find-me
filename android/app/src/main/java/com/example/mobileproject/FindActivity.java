16d78a7446d59f399ffac3a6ee9a62e5e98e4367
azmodessu <golovrkd@yandex.ru>
2024-04-09 20:33:56 +0300

    Successfully added getting list of users' profiles

--- android/app/src/main/java/com/example/mobileproject/FindActivity.java
+++ android/app/src/main/java/com/example/mobileproject/FindActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobileproject;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class FindActivity extends AppCompatActivity {
+
+    List<Profile> profilesList;
+    private int currentUserIndex = 0; // Индекс текущего пользователя в списке
+    User userContext = UserContext.getInstance().getUser();
+
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,41 @@ public class FindActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://176.109.111.92:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<List<Profile>> call = apiService.getUsers(userContext.getUsername());
+        call.enqueue(new Callback<List<Profile>>() {
+            @Override
+            public void onResponse(Call<List<Profile>> call, Response<List<Profile>> response) {
+                if(response.isSuccessful()) {
+                    profilesList = response.body();
+                    name = findViewById(R.id.textViewUsername);
+                    name.setText(profilesList.get(0).getName());
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Не саксес",
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Profile>> call, Throwable t) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Фейлур",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+//        name = findViewById(R.id.textViewUsername);
+//        name.setText(profilesList.getProfileList().get(0).toString());
+
     }
 }
