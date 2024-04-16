e358fa635dc501e1863a9b0976014e2391e58731
azmodessu <golovrkd@yandex.ru>
2024-04-16 18:54:24 +0300

    for chat

---
+++ android/app/src/main/java/Chat/Message.java
@@ -0,0 +1,40 @@
+package Chat;
+
+import java.util.Date;
+
+public class Message {
+    public String username;
+    public String textMessage;
+    private  long messageTime;
+
+    public Message() {}
+    public Message(String username, String textMessage) {
+        this.username = username;
+        this.textMessage = textMessage;
+        this.messageTime = new Date().getTime();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTextMessage() {
+        return textMessage;
+    }
+
+    public void setTextMessage(String textMessage) {
+        this.textMessage = textMessage;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
