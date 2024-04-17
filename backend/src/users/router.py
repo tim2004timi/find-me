79190ff483eb4af5a778c648fa7b69bf6075ba66
tim2004timi <tim2004timi@mail.ru>
2024-04-18 01:59:34 +0300

    Add service of user

--- backend/src/users/router.py
+++ backend/src/users/router.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from users import service
+
+router = APIRouter(tags=["Пользователи"])
+
+
+@router.get("/", response_model=)
+async def get_users():
+    return await service.get_users
---
+++ backend/src/users/service.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from sqlalchemy import select, Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from passlib.context import CryptContext
+
+from .models import User
+from .schemas import UserCreate
+
+
+async def get_users(session: AsyncSession) -> List[User]:
+    stmt = select(User).order_by(User.id)
+    result: Result = await session.execute(stmt)
+    users = result.scalars().all()
+    return list(users)
+
+
+async def det_user_by_id(session: AsyncSession, user_id: int) -> User | None:
+    return await session.get(User, user_id)
+
+
+async def create_user(
+    session: AsyncSession, user_in: UserCreate
+) -> User | None:
+    user = User(**user_in.model_dump())
+    session.add(user)
+    await session.commit()
+    # await session.refresh()
+    return user
