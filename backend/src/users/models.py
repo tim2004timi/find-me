477f403305100e916ad50341a7975f81e26d151a
tim2004timi <tim2004timi@mail.ru>
2024-04-18 02:48:54 +0300

    Add unique username to user. Make migration

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -6,7 +6,9 @@ from ..database import Base
 class User(Base):
     __tablename__ = "users"
 
-    username: Mapped[str] = mapped_column(index=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        index=True, nullable=False, unique=True
+    )
     hashed_password: Mapped[str] = mapped_column(nullable=False)
 
     @declared_attr
