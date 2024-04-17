1fe04138c6ef361efec6da2c1b3ff1c524f6937a
tim2004timi <tim2004timi@mail.ru>
2024-04-18 02:58:44 +0300

    Add srvices to profile

---
+++ backend/src/profiles/services.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from sqlalchemy import select, Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .models import Profile as ProfileORMModel
+from .schemas import ProfileCreate, Profile
+
+
+async def get_profiles(session: AsyncSession) -> List[Profile]:
+    stmt = select(Profile).order_by(Profile.id)
+    result: Result = await session.execute(stmt)
+    profiles = result.scalars().all()
+    return list(profiles)
+
+
+async def get_profile_by_id(session: AsyncSession, profile_id: int) -> Profile | None:
+    return await session.get(Profile, profile_id)
+
+
+async def create_profile(
+    session: AsyncSession, profile_in: ProfileCreate
+) -> Profile | None:
+    profile = ProfileORMModel(**profile_in.model_dump())
+    session.add(profile)
+    await session.commit()
+    # await session.refresh()
+    return profile
